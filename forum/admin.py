9fd6ff2f0e977222cb8c5e4ab1541c14b07d8699
Mauricio <mauriciornn@gmail.com>
2023-09-21 08:19:37 -0300

    atualização 21/09

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from forum.models import Post
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id','post_title', 'body_text', 'pub_date')
+    list_filter = ('pub_date',)
 # Register your models here.
